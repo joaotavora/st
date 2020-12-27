c53e566ac69dce582e8413e2c9a191f95f781a02
João Távora <joaotavora@gmail.com>
2020-12-27 15:15:26 +0000

    * config.def.h (font): Choose TamzenForPowerline font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "TamzenForPowerline:pixelsize=20";
 static int borderpx = 2;
 
 /*
