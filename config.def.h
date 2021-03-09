1d0ef33c1267435ce4e43dfdf907cd3dc6fec6cd
João Távora <joaotavora@gmail.com>
2021-03-09 00:04:51 +0000

    Slightly wider border because prettier

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "TamzenForPowerline:pixelsize=20";
-static int borderpx = 25;
+static int borderpx = 30;
 
 /*
  * What program is execed by st depends of these precedence rules:
