5201fcd383d51143b54a735950c50b1478b95c5d
João Távora <joaotavora@gmail.com>
2024-11-16 12:48:36 +0000

    * config.def.h (cols): Slightly wider (120 cols) by default

--- config.def.h
+++ config.def.h
@@ -205,7 +205,7 @@ static Rune stcursor = 0x2603; /* snowman ("☃") */
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
+static unsigned int cols = 120;
 static unsigned int rows = 24;
 
 /*
